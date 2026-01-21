c6715f96162f7ec058f189ec7fd72136b65398ad
Elias Lennheimer <lennheimer.elias@gmail.com>
2026-01-21 09:35:32 +0100

    added test to calculator's add methods

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class CalculatorTest {
+
+    @Test
+    void addTwoNumbersReturnsSum() {
+        int result = Calculator.add(1, 2);
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void addThreeNumbersReturnsSum() {
+        int result = Calculator.add(1, 2, 3);
+        assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    void addMultipleNumbersReturnsSum() {
+        int result = Calculator.add(1, 2, 3, 4);
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void noParametersReturnsZero() {
+        int result = Calculator.add();
+        assertThat(result).isEqualTo(0);
+    }
+}
