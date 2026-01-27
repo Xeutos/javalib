bdc3ea3c01c05028b422a7a9f3a89b98d5e99a99
Elias Lennheimer <lennheimer.elias@gmail.com>
2026-01-27 13:34:22 +0100

    Add Bowling game logic and basic tests

---
+++ src/main/java/org/example/Bowling.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Bowling {
+    private int score = 0;
+
+    public void roll(int pins){
+        score += pins;
+    }
+
+    public int score() {
+        return score;
+    }
+}
---
+++ src/test/java/org/example/BowlingTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BowlingTest {
+
+    @Test
+    void gutterGameGiveScoreZero() {
+        Bowling bowlingGame = new Bowling();
+
+        for (int i = 0; i < 20 ; i++) {
+            bowlingGame.roll(0);
+        }
+
+        assertThat(bowlingGame.score()).isEqualTo(0);
+    }
+
+    @Test
+    void onePinEachRollGivesScore20() {
+        Bowling bowlingGame = new Bowling();
+
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(1);
+        }
+
+        assertThat(bowlingGame.score()).isEqualTo(20);
+    }
+}
